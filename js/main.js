4631faa13ffb18a017d77febe5dc9b7dd601472b
MarjoryMatos <marjory_matos@hotmail.com>
2021-07-03 17:57:44 -0300

    Atividade JS
    Atividade realizada para identificar as funções de JS

---
+++ js/main.js
@@ -0,0 +1,82 @@
+
+
+/*function botao(){
+    document.getElementById("agradecimento");
+    console.log(document.getElementById("agradecimento").innerHTML="Obrigado por clicar");
+
+};
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML="Obrigado por passar";
+
+    //alert("trocar texto");
+}
+
+function funcaochange(elemento){
+    console.log(elementovalue)
+}*/
+/*function validaidade (idade){
+    if (idade >=18){
+    validar= true
+}else{
+    validar=false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validaidade(idade));*/
+
+
+/*function soma(n1,n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+alert(soma(3,10));
+alert(setReplace ("Vai Japão", "Japão", "Brasil"));*/
+
+
+/*var d = new Date();
+alert(d);*/
+
+/*var count = 0;
+for(count=0; count <= 5; count ++){
+    alert(count);
+};*/
+
+
+
+
+/*var count = 0;
+while(count < 5){
+    console.log(count);
+    count = count + 1;
+};*/
+
+
+
+/*var idade = prompt("Qual sua idade?")
+//var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};*/
+
+/*var fruta = {nome: "maçã", cor:"vermelho"};
+console.log(fruta);*/
+
+/*var nome=" Marjory Matos";
+var idade="26";
+alert(nome + " tem " + idade + " anos");*/
+
+//var lista=["maçã","pêra","Laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "))
