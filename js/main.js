876881ee52faee070848eeec92997ee9cb68c2c3
MarjoryMatos <marjory_matos@hotmail.com>
2021-07-04 14:47:14 -0300

    Programação para Internet com JS
    Estudo de uma das linguagens front-end, conteúdo contém exemplos de arrays, condicionais, laços de repetição e date. Além da manipulação de elementos da pagina

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
+//função
 
-
-/*function botao(){
+function botao(){
     document.getElementById("agradecimento");
     console.log(document.getElementById("agradecimento").innerHTML="Obrigado por clicar");
 
-};
+}; //botão que realiza alteração quando é clicado.
+
+/*para fazer a manipualção no elemento HTML ou CSS podemos utilizar o getElementby e identificamos o elemento desejado pelo ID definido no HTML */
 
 function trocar(){
     document.getElementById("mousemove").innerHTML="Obrigado por passar";
 
-    //alert("trocar texto");
-}
+    alert("trocar texto");
+} //função em que altera texto quando o mouser é passado por cima.
 
 function funcaochange(elemento){
-    console.log(elementovalue)
-}*/
-/*function validaidade (idade){
+    console.log(elementovalue) //é utilizado para que seja realizada determinada ação após alguma mudança.
+}
+
+function validaidade (idade){
     if (idade >=18){
     validar= true
 }else{
@@ -25,10 +28,10 @@ function funcaochange(elemento){
 }
 
 var idade = prompt("Qual sua idade?");
-console.log(validaidade(idade));*/
+console.log(validaidade(idade));//prompt recebemos algo do usurário
 
 
-/*function soma(n1,n2){
+function soma(n1,n2){
     return n1 + n2;
 }
 
@@ -37,46 +40,52 @@ function setReplace(frase, nome, novo_nome){
 }
 
 alert(soma(3,10));
-alert(setReplace ("Vai Japão", "Japão", "Brasil"));*/
+alert(setReplace ("Vai Japão", "Japão", "Brasil"));//Replace realiza a troca de Japão p/ Brasi.
+
+//date
+var d = new Date();
+alert(d); //informa data
 
 
-/*var d = new Date();
-alert(d);*/
-
-/*var count = 0;
+//laço de repetição Para
+var count = 0;
 for(count=0; count <= 5; count ++){
     alert(count);
-};*/
+};//realiza a contagem de um número até o outro. Determina um termino para que a contagem não vire um loop
 
 
 
-
-/*var count = 0;
+//laço de repetição enquanto
+var count = 0;
 while(count < 5){
     console.log(count);
     count = count + 1;
-};*/
+};//enquanto o valor não for maior que cinco o programa realizar a contagem acrescentando mais um numero
 
 
-
-/*var idade = prompt("Qual sua idade?")
-//var idade = 18;
+//condicionais IF (se)
+var idade = prompt("Qual sua idade?")
+var idade = 18;
 if (idade >= 18){
     alert("maior de idade");
 }else{
     alert("menor de idade");
-};*/
+}; //retorna valor verdadeiro ou falso
 
-/*var fruta = {nome: "maçã", cor:"vermelho"};
-console.log(fruta);*/
 
-/*var nome=" Marjory Matos";
+var nome=" Marjory Matos";
 var idade="26";
-alert(nome + " tem " + idade + " anos");*/
+alert(nome + "tem " + idade + " anos"); 
+// variavel var recebe string com o nome e idade, a contatenação é realizada pelo sinal de +
 
-//var lista=["maçã","pêra","Laranja"];
-//lista.push("uva");
-//lista.pop();
-//console.log(lista);
-//console.log(lista.toString());
-//console.log(lista.join(" - "))
+//dicionário
+var fruta = {nome: "maçã", cor:"vermelho"};
+console.log(fruta); 
+
+//array
+var lista=["maçã","pêra","Laranja"];
+lista.push("uva"); //insere elemento "uva" na ultima posição
+lista.pop(); //eleminação do ultimo elemento
+console.log(lista);//irá mostrar a lista inteira e serve para identificar quantos arrays temos
+console.log(lista.toString());//transforma em string
+console.log(lista.join(" - "))//separa os conteudos pelo simbolo desejado
